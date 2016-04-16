6377a39b0fdf21c2418451dff715842cec9e0b47
Hirohito Kato <hkato193@gmail.com>
2016-04-16 13:27:09 +0900

    Initial Commit

---
+++ HKLColorTable/HKLColorTable.h
@@ -0,0 +1,19 @@
+//
+//  HKLColorTable.h
+//  HKLColorTable
+//
+//  Created by 加藤寛人 on 2016/04/16.
+//  Copyright © 2016年 Hirohito Kato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HKLColorTable.
+FOUNDATION_EXPORT double HKLColorTableVersionNumber;
+
+//! Project version string for HKLColorTable.
+FOUNDATION_EXPORT const unsigned char HKLColorTableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HKLColorTable/PublicHeader.h>
+
+
